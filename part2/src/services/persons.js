0d96f7167a7b8947ef3ecd400da93a62ceb9af86
Priyanshu Pandey <122542895+priyanshu-18@users.noreply.github.com>
2026-01-02 02:59:35 +0530

    Delete part2/src/services/persons.js

--- part2/src/services/persons.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const baseUrl = 'http://localhost:3001/persons'
-
-const getAll = () => axios.get(baseUrl).then(res => res.data)
-const create = (newObject) => axios.post(baseUrl, newObject).then(res => res.data)
-const remove = (id) => axios.delete(`${baseUrl}/${id}`)
-
-export default { getAll, create, remove }
